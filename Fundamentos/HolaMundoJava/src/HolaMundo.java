ae6249951aa75854649f2e1fc348dbabe24627b8
Miriam Elizarraras <hello.tarantino@gmail.com>
2022-09-02 11:03:42 -0500

    add: HolaMundoJava

---
+++ Fundamentos/HolaMundoJava/src/HolaMundo.java
@@ -0,0 +1,13 @@
+/*
+ *Mi clase en Java
+ */
+
+/**
+ *Fundamentos - Java
+ * @miridelaluz
+ */
+public class HolaMundo {
+    public static void main (String args[]){
+    System.out.println("Hola Mundo desde Java");
+    }
+}
